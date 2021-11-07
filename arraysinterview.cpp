ecb615bb54fe7fe135eae166acfcf6a5f7cfa011
PRITISH RANJAN PANDA <85501658+pandapritish25@users.noreply.github.com>
2021-11-07 09:28:56 +0530

    Update arraysinterview.cpp

--- arraysinterview.cpp
+++ arraysinterview.cpp
@@ -658,3 +658,294 @@ int main()
   }
 
 }
+
+
+
+
+// some more good questions on arrays for mock inteerview such as amazon deshaw and etc with the pallete of my cp programming
+// pritishcf307
+
+#include<bits/stdc++.h>
+#include<math.h>
+#include<string.h>
+#include<vector>
+#include<stdlib.h>
+#define MOD 1000000007
+#define INF 1e18
+using namespace std;
+#define debug(x) cout << #x << " " << x << endl;
+#define debug1(x) cout << #x << " " << x << " ";
+typedef long long int ll;
+typedef long double lld;
+#define timeset ios_base::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+ 
+/*#define debug(x);
+printf(x);*/
+ 
+void init()
+{
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt" , "r"  ,stdin); 
+  freopen("output.txt" , "w" ,stdout);
+  #endif
+ 
+}
+ 
+void  debugger()
+{
+  #ifndef ONLINE_JUDGE
+  freopen("input.txt" , "r"  ,stdin); 
+  freopen("output.txt" , "w" ,stdout);
+  #endif
+  #ifdef debugger
+  #define debug(x) cerr << #x<<" "; _print(x); cerr << endl;
+  #else
+  #define debugger(x);
+  #endif
+} 
+ 
+ 
+const int MAXN = (int)((1e5) + 100);
+ll expo(ll a, ll b, ll mod) {ll res = 1; while (b > 0) {if (b & 1)res = (res * a) % mod; a = (a * a) % mod; b = b >> 1;} return res;}
+int gcd(int a, int b) { if (a == 0) return b; return gcd(b % a, a);}
+int max(int a, int b) {if (a > b) return a; else return b;}
+int min(int a, int b) {if (a < b) return a; else return b;}
+ 
+ 
+void precision(int a) 
+{
+  cout << setprecision(a) << fixed;
+}
+ 
+ 
+//** pritishcf307 **//
+//**------------------------------------------------------------------------------------------------------**//
+
+int main()
+{
+  init();
+  IOS;
+  ll n;
+  cin >> n;
+  vector<int> v;
+
+  ll sum=0;
+
+  for(int i=0;i<n;i++)
+  {
+    ll a;
+    cin >> a;
+    v.push_back(a);
+    sum+=a;
+  }
+
+  ll csum=(n)*(n+1);
+  csum/=2;
+
+  map<int,int> mp;
+
+  for(int i=0;i<n;i++)
+  {
+    mp[v[i]]++;
+  }
+  for(auto i:mp)
+  {
+    if(i.first > 1)
+    {
+      cout <<  i.second << endl;
+    }
+  }
+
+  cout << abs(csum-sum) << endl;
+}
+
+
+
+int main()
+{
+  init();
+  IOS;
+
+  ll n;
+  cin >> n;
+  ll a[n],b[n];
+
+  a[0]=1;
+  b[0]=1;
+
+  for(int i=1;i<n;i++)
+  {
+    a[i]=a[i-1]+b[i-1];
+    b[i]=a[i-1];
+  }
+
+  // for(int i=0;i<n;i++)
+  // {
+  //   debug(b[i]);
+  // }
+  cout << a[n-1]+b[n-1] << endl;
+}
+
+int main()
+{
+  init();
+  IOS;
+  ll n;
+  cin >> n;
+
+  vector<int> v;
+
+  for(int i=0;i<n;i++)
+  {
+    ll a;
+    cin >> a;
+    v.push_back(a);
+  }
+
+  map<int,int> mp;
+
+  for(int i=0;i<n;i++)
+  {
+    mp[v[i]]++;
+  }
+
+  ll p=n/2;
+
+  for(auto i:mp)
+  {
+    if(i.second >= p)
+    {
+      cout << i.first << endl;
+    }
+  }
+  return 0;
+}
+
+
+bool compare(int x,int y)
+{
+  if(abs(x) < abs(y))
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
+
+int main()
+{
+  init();
+  IOS;
+  ll n;
+  cin >> n;
+  ll a[n];
+
+  for(int i=0;i<n;i++)
+  {
+    cin >> a[i];
+  }
+
+  ll mn=INT_MAX;
+  sort(a,a+n,compare);
+
+  for(int i=1;i<n;i++)
+  {
+    if(a[i-1]+a[i] < mn)
+    {
+      mn=abs(a[i-1]+a[i]);
+    }
+  }
+
+  cout << mn << endl;
+}
+
+int main()
+{
+  init();
+  IOS;
+  ll n;
+  cin >> n;
+
+  vector<int> v;
+  vector<int> v1;
+
+  for(int i=0;i<n;i++)
+  {
+    ll a;
+    cin >> a;
+    v.push_back(a);
+  }
+
+  bool found=false;
+
+  for(int i=0;i<n;i++)
+  {
+    if(v[i] > 0)
+    {
+      v1.push_back(v[i]);
+    }
+  }
+
+  sort(v1.begin(),v1.end());
+
+  for(int i=1;i<v1.size();i++)
+  {
+    if(v1[i]!=v1[i-1]+1)
+    {
+      found=true;
+      cout << v1[i-1]+1;
+      break;
+    }
+  }
+
+  if(found==false)
+  {
+    cout << v[n-1]+1 << endl;
+  }
+
+}
+
+int main()
+{
+  init();
+  IOS;
+
+  ll n;
+  cin >> n;
+  ll k;
+  cin >> k;
+
+  vector<int> v;
+
+  for(int i=0;i<k;i++)
+  {
+    ll a;
+    cin >> a;
+    v.push_back(a);
+  }
+
+  ll count=0;
+  ll count2=0;
+
+  for(int i=1;i<=n;i++)
+  {
+    count=0;
+    for(int j=0;j<k;j++)
+    {
+      if(i%v[j]==0)
+      {
+        count++;
+      }
+    }
+    if(count==0)
+    {
+      count2++;
+    }
+  }
+  cout << count2 << endl;
+}
+
+
